90a614b42aa7c55ba31b9a09093a565384bb70a4
Kaifa Riyaz <kaifariyaz@gmail.com>
2026-05-14 20:17:28 +0530

    register

--- users/views.py
+++ users/views.py
@@ -16,7 +16,7 @@ def register(request):
         if form.is_valid():
             form.save()
             username = form.cleaned_data.get('username')
-            password = form.cleaned_data.get('password')
+            password = form.cleaned_data.get('password1')
             user = authenticate(username=username,password=password)
             login(request,user)
             return redirect('profile')
